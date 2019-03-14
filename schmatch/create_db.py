f1ad92eba9e988012efe2cea4c584c6b30e71569
Adrian Sampson <adrian@radbox.org>
2019-03-13 22:50:03 -0400

    More flexible DB creation function

--- schmatch/create_db.py
+++ schmatch/create_db.py
@@ -1,6 +1,13 @@
 import os
 from .app import app, db
 
-if __name__ == '__main__':
+
+def create(drop_first=False):
     os.makedirs(app.instance_path, exist_ok=True)
+    if drop_first:
+        db.drop_all()
     db.create_all()
+
+
+if __name__ == '__main__':
+    create()
